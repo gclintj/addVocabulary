2e2fb09e9554da0e62c2175d2a17f5404fdd34dd
lintj <gclintj@hotmail.com>
2021-03-06 22:26:56 -0700

    first success

--- main.py
+++ main.py
@@ -1,20 +1,30 @@
 import csv
 
 #use a file
-with open('file.csv', newline = '') as oldcsv:
-    oldlist = list(csv.reader(oldcsv))
+path = '/home/lintj/file.csv'
+with open(path, newline = '') as oldcsv:
+    oldlist = list(csv.reader(oldcsv))[0]
 
 #get new words
-#TODO
-Enter word(s) to be added, seperated by comma:
+words = input("Enter word(s) to be added, seperated by comma:\n")
 
 #split
-add_list = [w.strip() for w in word_string.split(',') if w.strip()]
+add_list = [w.strip() for w in words.split(',') if w.strip()]
+
+print(oldlist)
+print(add_list)
 
 #combine and remove duplicate
-
-list = set(old + list)
+new_list = list(set(oldlist + add_list))
 
 #sort
+new_list.sort()
+
+print(new_list)
 
 #save file
+with open(path, 'w', newline = '') as newcsv:
+    write = csv.writer(newcsv)
+    write.writerow(new_list)
+
+#to be tested: Chinese comma
